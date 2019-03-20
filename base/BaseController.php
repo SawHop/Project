28943b6f7cc22546ac6bd5e05743544ff675f652
Дмитрий Мухин <SawHo@DESKTOP-4H8896D>
2019-03-20 22:31:14 +0700

    Repeated the lecture

---
+++ base/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: SawHo
+ * Date: 24.03.2019
+ * Time: 14:14
+ */
+
+
+namespace app\base;
+
+use yii\web\Controller;
+
+class BaseController extends Controller
+{
+
+}
---
+++ modules/auth/models/SignUpModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SawHo
+ * Date: 24.03.2019
+ * Time: 18:30
+ */
+
+namespace modules\auth\controllers\models;
+
+
+use yii\base\Model;
+
+class SignUpModel extends Model
+{
+
+}
